193aa36ac67b00802fe8214e6ae2fd2fb3248668
JP Magalhaes <40191142+JPMagalhaesCESAR@users.noreply.github.com>
2018-06-14 02:33:22 -0300

    Adding Q2 palindrome file

---
+++ Q2/palindrome.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+/**
+ * Escreva uma função que analisa uma dada palavra ou frase, retornando se a mesma é um palíndromo ou não.
+ * 
+ * Função: bool isPalindrome (char[] sentence, int length)
+ * Input:
+ * - sentence: array de caracteres em minúsculo da palavra ou frase a ser analisada.
+ * - length: tamanho do array - número de caracteres
+ * Output: true se a sentença é um palídromo, falso caso negativo.
+ */
+
+bool isPalindrome (char sentence[], int length) {
+    printf("\nisPalindrome::START: %s, %d\n", s, length);
+
+    // TODO: YOUR CODE HERE!
+    
+    printf("\nisPalindrome::END\n");
+    return false;
+}
+
+int main()
+{
+    printf("\n### CESAR School :: Sistemas Digitais :: Coding1 :: Palindrome ###\n");
+    
+    // true cases
+    isPalindrome("ovo", 3);
+    isPalindrome("arara", 5);
+
+    // false cases
+    isPalindrome("cesar", 5);
+    isPalindrome("school", 6);
+
+    return 0;
+}
+ 
