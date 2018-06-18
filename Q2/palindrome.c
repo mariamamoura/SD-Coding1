5412f6e11fa71e55cb38d0164ebdaa67d71b9fe7
mariamamoura <mouramariama@gmail.com>
2018-06-18 20:58:43 -0300

    Questao2

--- Q2/palindrome.c
+++ Q2/palindrome.c
@@ -11,10 +11,23 @@
  * Output: true se a sentença é um palídromo, falso caso negativo.
  */
 
+#include <stdio.h>
+#include <stdbool.h>
+
 bool isPalindrome (char sentence[], int length) {
     printf("\nisPalindrome::START: %s, %d\n", sentence, length);
 
-    // TODO: YOUR CODE HERE!
+       for(int i = 0; i < length; i++){
+       if (sentence[i] != sentence[length - 1 - i]){ // o -1 faz com que leia a mensagem ao contrario e cheque se é a mesma que a mensagem original
+           printf("false");
+           printf("\nisPalindrome::END\n");
+           return false;
+       }
+   }
+   printf("true");
+   printf("\nisPalindrome::END\n");
+   return true;
+}
     
     printf("\nisPalindrome::END\n");
     return false;
